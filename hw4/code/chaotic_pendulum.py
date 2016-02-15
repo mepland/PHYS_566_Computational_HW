d75591eb7b5f9c67d40d155803c2e35c83c1018f
Matthew Berg Epland <matthew.epland@cern.ch>
2016-02-15 18:15:13 -0500

    end of day 1, worked on theory and code

--- hw4/code/chaotic_pendulum.py
+++ hw4/code/chaotic_pendulum.py
@@ -11,60 +11,50 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 ########################################################
-# Set fixed parameters, SI Units
-mass = 46*0.001 # mass of ball = 46 grams
-v0 = 70.0 # Initial velocity
-rho = 1.29 # Density of air (at sea level)
-A =  0.0014 # Frontal Area
-g = 9.80665 # g at sea level
+# Set fixed parameters [SI units]
+g = 9.80665 # g, gravity [m/s^2]
+l = 9.8 # l, length [m]
+gamma = 2.05 # gamma, dampening constant [s^-1]
+possible_alphaD = [0.2, 0.5, 1.2] # driving force amplitude [rad/s^2]
 
-vartheta0 = [60, 45, 30, 15, 9] # Initial launch angles (degrees)
-# Convert to radians
-for k in range(len(vartheta0)):
-	vartheta0[k] = vartheta0[k]*(np.pi/180.0)
+# Euler-Cromer method parameters
+Dt = 0.001 # Time step of TODO simulation
+max_Dtheta = np.pi/100.0 # TODO Maximum radial distance allowed between time steps
 
-# Drag Coefficients and transition velocity
-C = 0.5
-C_prime = 7.0
-v_transition = 14.0
-
-# Spin Constant eta defined in writeup
-eta = 0.25
-
-dt = 0.0005 # Time step of simulation, empirically set to satisfy max_r
-max_r = 0.05 # Maximum distance allowed between time steps
-# For comparison, Solving 0.0014 = 2 pi r^2 for half the surface area of a sphere gives us the balls radius r ~= 0.015
-
+# TODO list of 10 good driving frequencies for res plot
 
 ########################################################
 # Print out starting values
 print '\nBeginning chaotic_pendulum.py simulation'
-print 'Parameters have units are SI unless otherwise specified'
+print '\nFixed Parameters are:'
 print '---------------------------------------------'
+print 'g = %.5f [m/s^2]' % g
+print 'l = %.5f [m]' % l
+print 'Dampening Constant gamma = %.5f [s^-1]' % gamma
 
+print '\nPossible Driving Amplitudes alphaD [radians/s^2]'
+print possible_alphaD
 
-print '\nMass is: %.3f' % mass
-print 'Initial Velocity is: %.1f' % v0
-print 'Air Density Rho is: %.2f' % rho
-print 'Frontal Area A is: %.4f' % A
-print 'g is: %.5f' % g
-
-print '\nLaunch Angles Vartheta are: '
-for k in range(len(vartheta0)):
-	print '%.4f radians, %.1f degrees' % (vartheta0[k], vartheta0[k]*(180.0/np.pi))
-
-print '\nDrag Coefficients are C = %.1f and C\' = %.1f' % (C, C_prime)
-print 'Transition Velocity is: %.1f' % v_transition
-
-print '\nSpin Constant Eta is: %.2f' % eta
-
-
-
-
-print '\nSimulation Time Step is: %.5f' % dt
-print 'Simulation Max Step Size is: %.3f' % max_r
+print '\nSimulation Time Step = %.5f [s]' % Dt
+print 'Simulation Max Step Size = %.3f [radians]' % max_Dtheta
 print '---------------------------------------------'
 print '---------------------------------------------\n'
+
+
+########################################################
+# Define a function to create the output dir
+# If it already exists don't crash, otherwise raise an exception
+# Adapted from A-B-B's response to http://stackoverflow.com/questions/273192/in-python-check-if-a-directory-exists-and-create-it-if-necessary
+# Note in python 3.4+ 'os.makedirs(output_path, exist_ok=True)' would handle all of this...
+def make_path(path):
+	try: 
+	    os.makedirs(path)
+	except OSError:
+	    if not os.path.isdir(path):
+	        raise Exception('Problem creating output dir %s !!!\nA file with the same name probably already exists, please fix the conflict and run again.' % output_path)
+# end def for make_path
+
+
 
 ########################################################
 # Define time_step class to hold all the relevant parameters at a time step
@@ -72,184 +62,242 @@ class time_step:
 
     def __init__(self, time):
 	self.t = time
-        self.x = -99.0
-	self.y = -99.0
-	self.vx = -99.0
-	self.vy = -99.0
-	
-	# Return vartheta of the time step when called
-    def vartheta(self):
-	if self.x == -99.0 and self.y == -99.0 and self.vx == -99.0 and self.vy == -99.0: print 'WARNING COMPUTING THETA ON DEFAULT TIME STEP'
-	return np.arctan2(self.vy, self.vx)
+        self.theta = -99.0
+	self.omega = -99.0
 
-	# Return the speed v of the time step when called
-    def vmag(self):
-	if self.x == -99.0 and self.y == -99.0 and self.vx == -99.0 and self.vy == -99.0: print 'WARNING COMPUTING VMAG ON DEFAULT TIME STEP'
-	return np.sqrt(np.square(self.vx) + np.square(self.vy))
+	# save the raw theta coordinate, no bounds
+	self.rawtheta = -99.0
 
-	# Return spatial separation r from another time step
-    def r(self, time_step2):
-	if self.x == -99.0 and self.y == -99.0 and self.vx == -99.0 and self.vy == -99.0: print 'WARNING COMPUTING r WITH ON DEFAULT TIME STEP'
-	return np.sqrt( np.square(self.x - time_step2.x) + np.square(self.y - time_step2.y))
-    # Note: We could speed up the computation by saving r, vmag after it is called the first time
-    # then return the value we have. This will be more complex and cost more memory though
-    # The plot printing seems to be the bottleneck, so it's not worth changing
+	# save the sim parameters, only fill at t = 0
+	self.alphaD = -99.0
+	self.omegaD = -99.0
+	self.theta0 = -99.0
+	self.tmax = -99.0
+	self.sim_method = ''
+	self.lin_case = ''
+
+    # Define a function to set theta while forcing -pi < theta <= pi 
+    # WARNING do not use object.theta = X to set theta!!!
+    # There are no real private variables in python so I can't enforce this...
+    def set_theta(self, new_theta):
+	self.rawtheta = new_theta
+
+	while ( new_theta <= -np.pi) : new_theta = new_theta + 2.0*np.pi
+	while ( new_theta > np.pi) : new_theta = new_theta - 2.0*np.pi
+
+	self.theta = new_theta
+	return None # explicitly return none, python would implicitly otherwise
 
 # end class for time_step
 
 ########################################################
-# Define a function to run the simulation based on which case we are in and what vartheta0 we want
-def run_sim(case, m_vartheta0, m_color):
+# Define a function to return theta or sin(theta) depending on the lin_case 
+def linearity(theta, lin_case):
+	if(lin_case == 'linear'):
+		return theta
+	elif(lin_case == 'nonlinear'):
+		return np.sin(theta)
+# end def for linearity
+
+########################################################
+# Define a function to run the simulation
+def run_sim(alphaD, omegaD, theta0, periodsD, sim_method, lin_case):
 	
 	# Start the list of time_steps
 	run = [time_step(0.0)]
+
+	# save the sim parameters
+	run[0].alphaD = alphaD
+	run[0].omegaD = omegaD
+	run[0].theta0 = theta0
+	run[0].tmax = periodsD*(2.0*np.pi/omegaD)
+	run[0].sim_method = sim_method
+	run[0].lin_case = lin_case
 	
 	# Set the initial values
-	run[0].x = 0.0
-	run[0].y = 0.0
-	run[0].vx = v0*np.cos(m_vartheta0)
-	run[0].vy = v0*np.sin(m_vartheta0)
+	run[0].set_theta(theta0)
+	run[0].omega = 0.0
 
-	# Loop until we hit the ground
-	current_y = 99.0 # use to halt while loop
-	i = 0 # current time step
-	C_dimpled = C # Declare C_dimpled for use later, just set it to C for now
+	# Loop for periodsD driving force periods
+	n = 0 # time step index
 
-	while current_y > 0.0:
-		i = i + 1 # increment time step
-		run.append(time_step(i*dt)) # append a new time_step object
+
+	while( n*Dt <= run[0].tmax ):
+		# append a new time_step object for n+1
+		run.append(time_step((n+1)*Dt)) 
+
+		if(sim_method == 'euler_cromer'): # Euler-Cromer method
+
+			# Compute the new velocity, see writeup for details...
+			run[n+1].omega = run[n].omega + ( -(g/l)*linearity(run[n].theta, lin_case) -2*gamma*run[n].omega + alphaD*np.sin(omegaD*run[n].t) )*Dt
 	
-		# Compute the new position
-		run[i].x = run[i-1].x + run[i-1].vx*dt
-		run[i].y = run[i-1].y + run[i-1].vy*dt
+			# Compute the new position
+			run[n+1].set_theta( run[n].theta + run[n+1].omega*Dt )
 
-		# Compute the new velocity, see writeup for physics...
+		#if(sim_method == 'runge_kutta'): # Runge-Kutta method
 
-		if(case == 'a'): # ideal
-			run[i].vx = run[i-1].vx + (dt/mass)*( 0.0 )
-			run[i].vy = run[i-1].vy + (dt/mass)*( -mass*g )
+			# TODO
 
-		if(case == 'b'): # smooth ball with drag
-			F_drag_smooth = -C*rho*A*np.square(run[i-1].vmag())
-			run[i].vx = run[i-1].vx + (dt/mass)*( F_drag_smooth*np.cos(run[i-1].vartheta()) )
-			run[i].vy = run[i-1].vy + (dt/mass)*( F_drag_smooth*np.sin(run[i-1].vartheta()) -mass*g )
-
-		if(case == 'c'): # dimpled ball with drag
-			# see what v regime we are in and pick C accordingly
-			if( run[i-1].vmag() <= v_transition):
-				C_dimpled = C # 1/2
-			else:
-				C_dimpled = C_prime/run[i-1].vmag() # C'/v
-			F_drag_dimpled = -C_dimpled*rho*A*np.square(run[i-1].vmag())
-			run[i].vx = run[i-1].vx + (dt/mass)*( F_drag_dimpled*np.cos(run[i-1].vartheta()) )
-			run[i].vy = run[i-1].vy + (dt/mass)*( F_drag_dimpled*np.sin(run[i-1].vartheta()) -mass*g )
-
-		if(case == 'd'): # dimpled ball with drag and spin
-			# see what v regime we are in and pick C accordingly
-			if( run[i-1].vmag() <= v_transition):
-				C_dimpled = C # 1/2
-			else:
-				C_dimpled = C_prime/run[i-1].vmag() # C'/v
-			F_drag_dimpled = -C_dimpled*rho*A*np.square(run[i-1].vmag())
-			F_spin = mass*eta*run[i-1].vmag()
-			run[i].vx = run[i-1].vx + (dt/mass)*( -F_spin*np.sin(run[i-1].vartheta()) + F_drag_dimpled*np.cos(run[i-1].vartheta()) )
-			run[i].vy = run[i-1].vy + (dt/mass)*(  F_spin*np.cos(run[i-1].vartheta()) + F_drag_dimpled*np.sin(run[i-1].vartheta()) -mass*g )
 
 
 		# Make sure we didn't move to far, notify user and exit if we did
-		if(run[i].r(run[i-1]) > max_r):
-			print 'On time step %d the ball moved to far, r = %.3f\nProgram Exiting' % (i, run[i].r(run[i-1]))
+		Dtheta = abs(run[n+1].rawtheta - run[n].theta) # Use rawtheta to avoid branch cut problems
+		if( Dtheta > max_Dtheta):
+			print 'On time step %d the pendulum moved to far, Dtheta = %.3f\nProgram Exiting' % (n+1, Dtheta )
 			sys.exit()
-		current_y = run[i].y # update current_y
-	# end while current_y > 0.0 loop
-	
-	########################################################
-	# Create ndarrays that we can plot
-	
-	# Declare the x and y ndarrays based on our finished run's size
-	x_ndarray = np.zeros(len(run))
-	y_ndarray = np.zeros(len(run))
-	
-	# Fill the ndarrays
-	for j in range(len(run)):
-		x_ndarray[j] = run[j].x
-		y_ndarray[j] = run[j].y
-	
-	########################################################
-	# Create and return the scatter object
-	m_label = '$\\vartheta_{0}$ = %.1f$^{\circ}$' % (m_vartheta0*(180.0/np.pi))
-	return plt.scatter(x_ndarray, y_ndarray, marker='.', label=m_label, c=m_color, edgecolors='none')
+
+		n = n+1 # increment time step
+	# end while loop
+
+	print 'Run Completed!'
+
+	return run
 # end def for run_sim
 
-########################################################
-# Define a function to run the simulation multiple times for all the vartheta0's 
-def loop_vartheta0(case):
-	# Define the colors we want to use, include some extra to be safe
-	# If crimson starts printing, you better be careful!
-	colors = ['magenta', 'black', 'blue', 'green', 'cyan', 'crimson']
-	scatters = []
-	for k in range(len(vartheta0)):
-		print 'Running with vartheta0 = %.1f degrees' % (vartheta0[k]*(180.0/np.pi))
-		scatters.append(run_sim(case, vartheta0[k], colors[k]))
-	return scatters
-# end def for loop_vartheta0
+
 
 ########################################################
-# Define a function to run, plot, and print a whole case 
-def run_case(case, title, fname):
-	print '\nRunning Case: %s' % title
-	print '---------------------------------------------\n'
+# Define a function to take two runs and compare them
+# Runs should have the same driving force and run time
+# Should be used to compare Euler-Cromer to Runge-Kutta and linear to nonlinear
+def compare_runs(run1, run1_name, run2, run2_name, title, fname):
+	if( run1[0].alphaD != run2[0].alphaD and run1[0].omegaD != run2[0].omegaD and run1[0].tmax != run2[0].tmax ):
+		print 'SERIOUS ERROR: Comparing incompatible runs!!!!'
 
-	fig = plt.figure(case) # get a separate figure for the case
-	ax = fig.add_subplot(111) # Get the axes, effectively
+	########################################################
+	# Create ndarrays that we can plot
+	num_timesteps = len(run1)
+	
+	t_ndarray = np.zeros(num_timesteps)
 
-	loop_vartheta0(case) # generate the scatters
+	driving_theta = np.zeros(num_timesteps)
 
-	##########################
-	# Format the plot
+	run1_theta = np.zeros(num_timesteps)
+	run1_omega = np.zeros(num_timesteps)
+	run1_kenetic = np.zeros(num_timesteps)
+	run1_potential = np.zeros(num_timesteps)
+	run1_total = np.zeros(num_timesteps)
 
-	ax.set_title(title)
+	run2_theta = np.zeros(num_timesteps)
+	run2_omega = np.zeros(num_timesteps)
+	run2_kenetic = np.zeros(num_timesteps)
+	run2_potential = np.zeros(num_timesteps)
+	run2_total = np.zeros(num_timesteps)
 
-	ax.set_xlabel('$x$ [m]')
-	ax.set_ylabel('$y$ [m]')
+	# Fill the ndarrays
+	mass = 1.0 # put mass in formulas just out of habit
+	# energy units will be energy/mass
+	for i in range(num_timesteps):
+		t_ndarray[i] = run1[i].t
 
-	ax.set_xlim((0.0, None))
-	ax.set_ylim((0.0, None))
+		driving_theta[i] = run1[0].alphaD*np.sin(run1[0].omegaD*run1[i].t) 
 
+		run1_theta[i] = run1[i].theta
+		run1_omega[i] = run1[i].omega
+		run1_kenetic[i] = 0.5*mass*l*l*np.square(run1[i].omega)
+		run1_potential[i] = mass*g*l*(1.0-np.cos(run1[i].theta))
+		run1_total[i] = run1_kenetic[i] + run1_potential[i]
+
+		run2_theta[i] = run2[i].theta
+		run2_omega[i] = run2[i].omega
+		run2_kenetic[i] = 0.5*mass*l*l*np.square(run2[i].omega)
+		run2_potential[i] = mass*g*l*(1.0-np.cos(run2[i].theta))
+		run2_total[i] = run2_kenetic[i] + run2_potential[i]
+
+
+	########################################################
+	# Create the plots
+	m_path = output_path+'/'+fname
+	make_path(m_path)
+
+	# theta
+	theta_fig = plt.figure('theta') # get a separate figure
+	theta_ax = theta_fig.add_subplot(111) # Get the axes, effectively
+
+	plt.plot(t_ndarray, run1_theta, ls='solid', label=run1_name, c='blue')
+	plt.plot(t_ndarray, run2_theta, ls='solid', label=run2_name, c='green')
+	plt.plot(t_ndarray, driving_theta, ls='dotted', label='$F_{Driving}$', c='black')
+
+	theta_ax.set_title(title)
+	theta_ax.set_xlabel('$t$ [s]')
+	theta_ax.set_ylabel('$\\theta$ [radians]')
+	theta_ax.set_xlim((0.0, run1[0].tmax))
 	plt.legend()
 
-	##########################
-	# Print the plot
-	# fig.savefig(output_path+'/'+fname+'.pdf')
-	# Due to the number of points pdfs will be VERY large
-	# So instead only make high quality pngs
-	fig.savefig(output_path+'/'+fname+'.png', dpi=900)
+	theta_fig.savefig(m_path+'/'+fname+'_theta.pdf')
 
-	print 'Case Completed'
-# end def for run_case
+	# run1 energy
+	run1_energy_fig = plt.figure('run1_energy') # get a separate figure
+	run1_energy_ax = run1_energy_fig.add_subplot(111) # Get the axes, effectively
+
+	plt.plot(t_ndarray, run1_kenetic, ls='solid', label='$K$', c='blue')
+	plt.plot(t_ndarray, run1_potential, ls='solid', label='$U$', c='green')
+	plt.plot(t_ndarray, run1_total, ls='solid', label='$E$', c='black')
+
+	run1_energy_ax.set_title('Energy: '+run1_name)
+	run1_energy_ax.set_xlabel('$t$ [s]')
+	run1_energy_ax.set_ylabel('$E$ [J/mass]')
+	run1_energy_ax.set_xlim((0.0, run1[0].tmax))
+	plt.legend()
+
+	run1_energy_fig.savefig(m_path+'/'+fname+'_'+run1_name+'_energy.pdf')
+
+
+	# run2 energy
+	run2_energy_fig = plt.figure('run2_energy') # get a separate figure
+	run2_energy_ax = run2_energy_fig.add_subplot(111) # Get the axes, effectively
+
+	plt.plot(t_ndarray, run2_kenetic, ls='solid', label='$K$', c='blue')
+	plt.plot(t_ndarray, run2_potential, ls='solid', label='$U$', c='green')
+	plt.plot(t_ndarray, run2_total, ls='solid', label='$E$', c='black')
+
+	run2_energy_ax.set_title('Energy: '+run2_name)
+	run2_energy_ax.set_xlabel('$t$ [s]')
+	run2_energy_ax.set_ylabel('$E$ [J/mass]')
+	run2_energy_ax.set_xlim((0.0, run1[0].tmax))
+	plt.legend()
+
+	run2_energy_fig.savefig(m_path+'/'+fname+'_'+run2_name+'_energy.pdf')
+
+
+
+
+	print 'compare_runs completed'
+# end def for compare_runs
 
 
 
 ########################################################
-# Create the output dir, if it already exists don't crash, otherwise raise an exception
-# Adapted from A-B-B's response to http://stackoverflow.com/questions/273192/in-python-check-if-a-directory-exists-and-create-it-if-necessary
-# Note in python 3.4+ 'os.makedirs(output_path, exist_ok=True)' would handle all of this...
+########################################################
+########################################################
+# Finally, actually run things!
+
 output_path = './output' # Set output path, hard coded...
-try: 
-    os.makedirs(output_path)
-except OSError:
-    if not os.path.isdir(output_path):
-        raise Exception('Problem creating output dir %s !!!\nA file with the same name probably already exists, please fix the conflict and run again.' % output_path)
 
-########################################################
-# Finally, actually run over the four cases
+# vary theta0
+'''
+# run_sim(alphaD, omegaD, theta0, periodsD, sim_method, lin_case)
 
-run_case('a', 'Ideal Ball', 'ideal')
-run_case('b', 'Smooth Ball with Drag', 'smooth_ball_w_drag')
-run_case('c', 'Dimpled Ball with Drag', 'dimpled_ball_w_drag')
-run_case('d', 'Dimpled Ball with Drag and Spin', 'dimpled_ball_w_drag_and_spin')
+ec_run = run_sim(possible_alphaD[0], 1.0, 0.0, 25, 'euler_cromer','linear')
+ec_run2 = run_sim(possible_alphaD[0], 1.0, 20.0*(np.pi/180.0), 25, 'euler_cromer','linear')
+
+# compare_runs(run1, run1_name, run2, run2_name, title, fname) 
+compare_runs(ec_run, '$\\theta_{0} = 0.0^{\circ}$', ec_run2, '$\\theta_{0} = 10.0^{\circ}$', 'Vary $\\theta_{0}$', 'vary_theta0')
+'''
+
+# vary linearity 
+# run_sim(alphaD, omegaD, theta0, periodsD, sim_method, lin_case)
+
+ec_run = run_sim(possible_alphaD[0], 1.0, np.pi, 25, 'euler_cromer','linear')
+ec_run2 = run_sim(possible_alphaD[0], 1.0, np.pi, 25, 'euler_cromer','nonlinear')
+
+# compare_runs(run1, run1_name, run2, run2_name, title, fname) 
+compare_runs(ec_run, 'Linear', ec_run2, 'Nonlinear', 'Vary Linearity', 'vary_linearity')
+
+
 
 
 ########################################################
 print '\n\nDone!\n'
+
 
