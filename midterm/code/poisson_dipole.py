6731f7ab42c1c8196494c1e40ecf6cc46f96067e
Matthew Berg Epland <matthew.epland@cern.ch>
2016-02-26 17:28:46 -0500

    plotting written up

--- midterm/code/poisson_dipole.py
+++ midterm/code/poisson_dipole.py
@@ -5,13 +5,14 @@ import os
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+#import matplotlib as mpl # We need to import all of matplotlib just to set rcParams once...
 from matplotlib.ticker import MultipleLocator
 from scipy.optimize import curve_fit
 
 ########################################################
 # Set fixed/global parameters [SI units]
 
-Q_over_epsilon0 = 1.0 # point charge Q over epsilon0 [V m]
+Q_over_epsilon0 = 1.0 # point charge Q over epsilon0 [Vm]
 
 # TODO Run values
 a = 0.6 # dipole separation length [m]
@@ -27,7 +28,7 @@ print '\nBeginning poisson_dipole.py'
 print '\nFixed Parameters are:'
 print '---------------------------------------------'
 
-print '\nQ over epsilon0 = %.1f [V m]' % Q_over_epsilon0
+print '\nQ over epsilon0 = %.1f [Vm]' % Q_over_epsilon0
 print 'dipole seperation a = %.1f [m]' % a
 print 'Circular boundary R = %.1f [m]' % R
 
@@ -52,13 +53,15 @@ def make_path(path):
 
 ########################################################
 # Define a function to run the simulation
-def run_sim(L, Dx, sim_method, halt_method, epsilon, fixed_accuracy):
-	
+def run_sim(L, Dx, sim_method, halt_method, epsilon, fixed_accuracy, debugging):
+	print 'Beginning run_sim:'	
+
 	# Find the index of the center/origin
 	l_center = (L-1)/2 # +1 to get to center, -1 as we start from 0
 	
 	########################################################
 	# Define a function to take ndarray indexes and return spatial coordinates of the BIN CENTERS 
+	# TODO move out of this function?
 	def find_spatial(y_index,x_index):
 		# X: 0 ~ - R, l_center=(L-1)/2 ~ 0.0, L-1 ~ R
 		# y: 0 ~ R, l_center=(L-1)/2 ~ 0.0, L-1 ~ -R
@@ -127,16 +130,16 @@ def run_sim(L, Dx, sim_method, halt_method, epsilon, fixed_accuracy):
 	V[l_center][l_left_Q] = -Q_over_epsilon0
 	V[l_center][l_right_Q] = Q_over_epsilon0
 	
-	#TODO Debugging
-	# print V
-	
+	# Print the initial V if debugging
+	if(debugging): plot_V(' Initial', 'initial', V, 0, 10, './output/poisson_dipole', L, Dx, sim_method, halt_method, epsilon, fixed_accuracy)
+
 	########################################################
 	# Define a function to perform a sweep through the whole ndarray
 	# using the Jacobi relaxation algorithm
 	def jacobi_sweep(m_V):
 		V_old = m_V.copy() # save the current values
 	
-		tol_sum = 0.0 # to find average abs percent tolerance, numerator of average
+		tol_sum = 0.0 # to find average abs tolerance, numerator of average
 		sim_points = 0 # number of points that got updated, denominator of average
 		# we need to define this because at the start only the neighbors of the dipole
 		# points will change, and we don't want 0 -> 0 far away to count as being convergent,
@@ -151,20 +154,8 @@ def run_sim(L, Dx, sim_method, halt_method, epsilon, fixed_accuracy):
 					# Update from V_old, Jacobi method = average nearest neighbors
 					m_V[l_y][l_x] = 0.25*( get_value(V_old,l_y,l_x-1) + get_value(V_old,l_y-1,l_x) + get_value(V_old,l_y,l_x+1) + get_value(V_old,l_y+1,l_x) )
 					# Compute the contribution to the tolerance for this nonboundary cell
-					if(V_old_value != m_V[l_y][l_x]):
-						if(m_V[l_y][l_x] != 0.0):
-							# Make sure we don't divide by zero!
-							tol_sum += abs((V_old_value - m_V[l_y][l_x])/m_V[l_y][l_x])	
-							sim_points += 1
-						else:
-							if(l_x != l_center):
-								# We should have V = 0 along the x center, and the simulation will ocassionally hit it exactly
-								# it should pretty much never happen elsewhere, print out a warning if it does to be safe
-								pos = find_spatial(l_y,l_x)
-								print 'WARNING AT (y,x)=(%2.3f, %2.3f): Vnew=0, Vold = %.3E, DROPPING THIS POINT FROM THE TOLERANCE CALC' % (pos[0], pos[1], V_old_value)
-		# TODO Debugigng
-		# print V_old
-		# print 'sim_points = %d' % sim_points
+					tol_sum += abs(V_old_value - m_V[l_y][l_x])
+					sim_points += 1
 
 		tol_sweep = tol_sum/sim_points
 		return [m_V, tol_sweep]
@@ -177,7 +168,10 @@ def run_sim(L, Dx, sim_method, halt_method, epsilon, fixed_accuracy):
 	sweep = [V, 2*epsilon]
 
 	if(halt_method == 'epsilon'):
-		while sweep[1] >= epsilon:
+		while (sweep[1] >= epsilon or n_sweep < l_center+1):
+			# Force it to run for at least l_center+1 # of times so that
+			# there is a chance for the origin to propagate out to the edge
+			# otherwise wait till the tolerance is low enough
 			if(sim_method == 'jacobi'):
 				sweep = jacobi_sweep(sweep[0])
 			#if(sim_method == 'SOR'):
@@ -186,14 +180,14 @@ def run_sim(L, Dx, sim_method, halt_method, epsilon, fixed_accuracy):
 				print 'ERROR! Unknown sim_method! Exiting!'
 				sys.exit()
 			n_sweep += 1
-			print 'Sweep number = %5d, tol_current = %.4f' % (n_sweep, sweep[1]) # TODO debugging
+			if(debugging): print 'Sweep number = %5d, tol_current = %.5E' % (n_sweep, sweep[1])
 	#elif(halt_method == 'fixed_accuracy'):
 		# TODO
 	else:
 		print 'ERROR! Unknown halt_method! Exiting!'
 		sys.exit()
 
-
+	print 'run_sim completed'
 	return [sweep[0], n_sweep] # TODO keep returning the info we need 
 # end def for run_sim
 
@@ -201,11 +195,63 @@ def run_sim(L, Dx, sim_method, halt_method, epsilon, fixed_accuracy):
 
 ########################################################
 # Define a function to plot V 
-def plot_V(m_V, n_sweep, L, Dx, sim_method, halt_method, epsilon, fixed_accuracy):
+def plot_V(optional_title, fname, m_V, n_sweep, n_contours, m_path, L, Dx, sim_method, halt_method, epsilon, fixed_accuracy):
+	print 'Beginning plot_V:'	
 	
+	# Define arrays to make meshgrid 
+	y = np.linspace(R,-R,L)
+	x = np.linspace(-R,R,L)
+	X,Y = np.meshgrid(x,y)
 
+	# Set up the figure and axes
+        fig = plt.figure('fig')
+        ax = fig.add_subplot(111)
+        ax.set_title('$V\left(x,y\\right)$'+optional_title)
+        ax.set_xlabel('$x$ [m]')
+        ax.set_ylabel('$y$ [m]')
+
+	# I like the default inwards ticks
+	#mpl.rcParams['xtick.direction'] = 'out'
+	#mpl.rcParams['ytick.direction'] = 'out'
+
+	# Make a contour plot
+	CS = ax.contour(X,Y,m_V,n_contours, antialiased=True)
+
+	# Set the contour labels
+	ax.clabel(CS, inline=1, fontsize=10, fmt='%2.2f')
+
+	# Set the color bar
+	CB = plt.colorbar(CS, shrink=0.8, extend='both', filled=True, cmap='inferno', ax=ax, label='$V\left(x,y\\right)$ [V]')
+
+	# If it's the initial/diagnostics plot, draw vertical dashed lines at +-a
+	if fname == 'initial':
+		ax.axvline(x=a/2, ls = 'dashed', label='$a/2$', c='red')
+		ax.axvline(x=-a/2, ls = 'dashed', label='$-a/2$', c='blue')
+
+	# Add a dashed circle for the R boundary condition
+	boundary_circle = plt.Circle((0,0), R, ls='dashed', color='grey',fill=False)
+	ax.add_artist(boundary_circle)
+
+
+	# Annotate
+	text = '$L =$ %3.d, $\Delta x =$ %.3f [m]' % (L, Dx)
+	text += '\n$R =$ %2.1f [m], $a =$ %1.1f [m]' % (R, a)
+	text += '\n$Q/\epsilon_{0} =$ %1.1f [Vm]' % (Q_over_epsilon0)
+	if(sim_method == 'jacobi'): text += '\nSim. Method = Jacobi'
+	if(sim_method == 'SOR'): text += '\nSim. Method = SOR'
+	if(halt_method == 'epsilon'): text += '\nConv. Criteria: $\epsilon <$ %.2E' % (epsilon)
+ 	if(halt_method == 'fixed_accuracy'): text += '\nConv. Criteria: $A <$ %.2E' % (fixed_accuracy)
+	if(optional_title != ' Initial'): text += '\n$N_{\mathrm{iter}} =$ %5.d' % (n_sweep)
+	plt.figtext(0.145, 0.13, text, bbox=dict(edgecolor='black', facecolor='white', fill=True), size='x-small')
+
+	# Print it out
+	make_path(m_path)
+	fig.savefig(m_path+'/V_'+fname+'.pdf')	
+
+	fig.clf() # Clear fig for reuse
+
+	print 'V plot printed'
 # end def for plot_V
-
 
 
 ########################################################
@@ -220,8 +266,7 @@ output_path = './output/poisson_dipole'
 
 ############################
 # TODO move these parameters to a higher level function
-Dx = 0.2 # Set the spacing of the grid points
-# Dx = 1.0
+Dx = 0.1 # Set the spacing of the grid points
 
 # Set the number of grid points
 L = int(round(2*R/Dx))
@@ -229,7 +274,7 @@ if(L % 2 == 0): L += 1 # ensure L is odd
 print 'L = %d' % L
 
 # set the tolerance
-epsilon = 0.01 # average, absolute percent change sweep to sweep
+epsilon = 0.00001 # average, absolute change sweep to sweep
 
 sim_method = 'jacobi'
 halt_method = 'epsilon'
@@ -238,9 +283,10 @@ fixed_accuracy = -99.0
 # TODO print the sim values, to the plot probably
 
 # Run the sim
-m_run = run_sim(L, Dx, sim_method, halt_method, epsilon, fixed_accuracy)
+m_run = run_sim(L, Dx, sim_method, halt_method, epsilon, fixed_accuracy, True)
 
-print m_run[0]
+#print m_run[0]
+plot_V('', 'final', m_run[0], m_run[1], 50, output_path, L, Dx, sim_method, halt_method, epsilon, fixed_accuracy)
 
 
 ########################################################
@@ -255,23 +301,4 @@ if(False):
 ########################################################
 print '\n\nDone!\n'
 
-
-'''
-# Display simple V for diagnostics
-# Fill the center with -99 to mark it, temporarily...
-V[l_center][l_center] = -99.0
-# Fill the corners with 1,2,3,4 to mark them, temporarily...
-V[0][0] = 1.0
-V[0][L-1] = 2.0
-V[L-1][L-1] = 3.0
-V[L-1][0] = 4.0
-# Fill the charges with +-7 to mark them, temporarily...
-V[l_center][l_left_Q] = -7.0
-V[l_center][l_right_Q] = 7.0
-print '1.0 point = (y,x) = (%.3f,%.3f)' % (find_spatial(0,0)[0], find_spatial(0,0)[1])
-print '2.0 point = (y,x) = (%.3f,%.3f)' % (find_spatial(0,L-1)[0], find_spatial(0,L-1)[1])
-print '3.0 point = (y,x) = (%.3f,%.3f)' % (find_spatial(L-1,L-1)[0], find_spatial(L-1,L-1)[1])
-print '4.0 point = (y,x) = (%.3f,%.3f)' % (find_spatial(L-1,0)[0], find_spatial(L-1,0)[1])
-print V
-'''
 
