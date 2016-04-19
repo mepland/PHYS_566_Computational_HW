ec11fd51fc8abc5c98c5f557f4bf58cc412af7ee
Matthew Epland <matthewepland@gmail.com>
2016-04-19 13:59:28 -0400

    have my parameters working

--- final/code/sweepmodule.c
+++ final/code/sweepmodule.c
@@ -1,112 +1,72 @@
 // adapted from http://blog.debao.me/2013/04/my-first-c-extension-to-numpy/
 // and http://docs.scipy.org/doc/numpy-1.10.1/user/c-info.how-to-extend.html
 
+// compile and install module with:
+// python module_setup_for_sweep.py install
+// or TODO
+// python module_setup_for_sweep.py build_ext --inplace
+
+
+/*
+test code
+
+import sweep
+import numpy as np
+a = np.array([[1.1, 2.2, 3.3], [1.2, 1.3, 1.4]])
+a = np.array([[1.1, 1.2, 1.3], [2.1, 2.2, 2.3], [3.1, 3.2, 3.3]])
+
+sweep.run_sweep(10.0, 0, a)
+print a
+
+sweep.run_sweep(10.0, 1, a)
+print a
+
+exit()
+
+*/
+
 #include "Python.h"
 #include "numpy/arrayobject.h"
 
-/*
 static PyObject*
 run_sweep (PyObject *dummy, PyObject *args)
 {
-    PyObject *arg1=NULL;
-    PyObject *arr1=NULL;
-    int nd;
+    double T;
+    int NN_type;
 
-    if (!PyArg_ParseTuple(args, "O", &arg1))
-        return NULL;
+    PyObject *out=NULL;
+    PyArrayObject *oarr=NULL;
 
-    arr1 = PyArray_FROM_OTF(arg1, NPY_DOUBLE, NPY_IN_ARRAY);
-    if (arr1 == NULL)
-        return NULL;
-
-    nd = PyArray_NDIM(arr1);   //number of dimensions
-
-    Py_DECREF(arr1);
-
-
-
-    return PyInt_FromLong(nd);
-}
-*/
-
-/*
-static PyObject *
-run_sweep(PyObject *dummy, PyObject *args)
-{
-    PyObject *arg1=NULL, *arg2=NULL, *arg3=NULL, *out=NULL;
-    PyObject *oarr=NULL;
-
-    if (!PyArg_ParseTuple(args, "OOOO!", &arg1, &arg2, &arg3,
+    if (!PyArg_ParseTuple(args, "diO!", &T, &NN_type,
         &PyArray_Type, &out)) return NULL;
 
-    oarr = PyArray_FROM_OTF(out, NPY_DOUBLE, NPY_INOUT_ARRAY);
-    if (oarr == NULL) goto fail;
-
-     code that makes use of arguments
-     You will probably need at least
-       nd = PyArray_NDIM(<..>)    -- number of dimensions
-       dims = PyArray_DIMS(<..>)  -- npy_intp array of length nd
-                                     showing length in each dim.
-       dptr = (double *)PyArray_DATA(<..>) -- pointer to data.
-
-       If an error occurs goto fail.
-
-
-
-    PyArray_GETPTR3 (E, i, j)
-
-
-
-    Py_DECREF(oarr);
-    Py_INCREF(Py_None);
-    return Py_None;
-
- fail:
-    PyArray_XDECREF_ERR(oarr);
-    return NULL;
-}
-*/
-
-static PyObject*
-run_sweep (PyObject *dummy, PyObject *args)
-{
-    PyObject *arg1=NULL, *arg2=NULL, *out=NULL;
-    PyArrayObject *arr1=NULL, *arr2=NULL, *oarr=NULL;
-
-    if (!PyArg_ParseTuple(args, "OOO!", &arg1, &arg2,
-        &PyArray_Type, &out)) return NULL;
-
-    arr1 = (PyArrayObject*)PyArray_FROM_OTF(arg1, NPY_DOUBLE, NPY_IN_ARRAY);
-    if (arr1 == NULL) return NULL;
-    arr2 = (PyArrayObject*)PyArray_FROM_OTF(arg2, NPY_DOUBLE, NPY_IN_ARRAY);
-    if (arr2 == NULL) goto fail;
     oarr = (PyArrayObject*)PyArray_FROM_OTF(out, NPY_DOUBLE, NPY_INOUT_ARRAY);
     if (oarr == NULL) goto fail;
 
     /*vv* code that makes use of arguments *vv*/
 
-    int nd = PyArray_NDIM(arr1);   //number of dimensions
-    npy_intp *shape = PyArray_DIMS(arr1);  // npy_intp array of length nd showing length in each dim.
-
     int i;
     int j;
-    for (i=0; i<oarr->dimensions[0]; ++i) {
-        for (j=0; j<oarr->dimensions[1]; ++j) {
+    for (i=0; i < oarr->dimensions[0]; ++i) {
+        for (j=0; j < oarr->dimensions[1]; ++j) {
             double *v = (double*)PyArray_GETPTR2(oarr, i, j);
-            *v = *v * 2;
+            if(NN_type == 0){
+              *v = *v + T;
+            }
+            else if(NN_type == 1){
+              *v = *v + 2*T;
+            }
+
         }
     }
     /*^^* code that makes use of arguments *^^*/
 
-    Py_DECREF(arr1);
-    Py_DECREF(arr2);
+
     Py_DECREF(oarr);
     Py_INCREF(Py_None);
     return Py_None;
 
  fail:
-    Py_XDECREF(arr1);
-    Py_XDECREF(arr2);
     PyArray_XDECREF_ERR(oarr);
     return NULL;
 }
